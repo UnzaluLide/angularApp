f43e0ec62472ff53414e9f109df21135bb93e821
unzalulide <lidiagonzalolopez@gmail.com>
2020-01-02 17:41:43 +0100

    se saca el array de clientes a una clase json. Se importa desde el componente clientes

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,14 @@
+import { Cliente } from "./cliente";
+
+export const CLIENTES:Cliente[]=[
+    {id:1, nombre:'Kimetz', apellido:'Zenitagoia', email: 'kzenitagoia@gmail.com', fecha:'02/02/1984'},
+    {id:2, nombre:'Maddi', apellido:'Bilbao', email: 'mbilbao@gmail.com', fecha:'24/09/1994'},
+    {id:3, nombre:'Lur', apellido:'Agirre', email: 'laguirre@gmail.com', fecha:'17/07/1987'},
+    {id:4, nombre:'Garazi', apellido:'Garmendia', email: 'ggarmendia@gmail.com', fecha:'02/09/1987'},
+    {id:5, nombre:'Lide', apellido:'Unzalu', email: 'lunzalu@gmail.com', fecha:'17/09/1987'},
+    {id:6, nombre:'Onintza', apellido:'Enbeita', email: 'oenbeita@gmail.com', fecha:'19/05/1979'},
+    {id:7, nombre:'Aritz', apellido:'Aranburu', email: 'aaranburu@gmail.com', fecha:'02/06/1987'},
+    {id:8, nombre:'Mikel', apellido:'Garitaonaindia', email: 'mgari@gmail.com', fecha:'04/04/1984'},
+    {id:9, nombre:'Arantza', apellido:'Orbegozo', email: 'aorbegozo@gmail.com', fecha:'19/08/1967'},
+    {id:10, nombre:'Lidia', apellido:'Gonzalo', email: 'lgonzalo@gmail.com', fecha:'17/09/1987'},
+  ];
